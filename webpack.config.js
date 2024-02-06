62fa50346c61d5860d660e12fca11faa2a00dc0e
Neil <88711171+21Neil@users.noreply.github.com>
2024-02-06 16:54:11 +0800

    Add home content to index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
-module.export = {
+module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.ttf$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
