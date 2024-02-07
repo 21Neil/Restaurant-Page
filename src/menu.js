021dbd5589296834eed47bf6b3886336e74db147
Neil <88711171+21Neil@users.noreply.github.com>
2024-02-07 15:54:28 +0800

    Finish Menu.js basic skeleton

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,42 @@
 export default function menu() {
-  console.log('menu')
+  const content = document.querySelector('#content');
+
+  const menuH2 = document.createElement('h2');
+
+  const createSection = (header) => {
+    const section = document.createElement('section')
+    const headerElement = document.createElement('h2')
+    section.appendChild(headerElement)
+    headerElement.textContent = header
+    return section
+  };
+  const appetizerSection = createSection('Appetizer');
+  const mainDishSection = createSection('Main Dish');
+  const drinkSection = createSection('Drink');
+
+  content.appendChild(appetizerSection);
+  content.appendChild(mainDishSection);
+  content.appendChild(drinkSection);
+
+  const createArticle = (header, parent, img) => {
+    const article = document.createElement('article');
+    const headerElement = document.createElement('h3');
+
+    article.appendChild(headerElement);
+
+    if (img) {
+      const imgElement = document.createElement('img');
+      article.appendChild(imgElement);
+    }
+
+    headerElement.textContent = header;
+    parent.appendChild(article);
+  };
+
+  createArticle('Caesar Salad', appetizerSection);
+  createArticle('Borscht', appetizerSection);
+  createArticle('New Yorker Steak', mainDishSection);
+  createArticle('Roast Pork Neck', mainDishSection);
+  createArticle('Red Wine', drinkSection);
+  createArticle('Margaret', drinkSection);
 }
